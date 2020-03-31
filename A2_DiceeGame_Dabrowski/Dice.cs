58779d01a38dc7ac01ad0f3be239bfe684d7c327
Dylan Dabrowski <dabrowsd@shernet.sheridancollege.ca>
2020-03-31 19:35:57 -0400

    Dice class made with two properties, Face and Max. Face being a read-only prop representing the face of the die when rolled, and Max being the max number the face can be (ex. 6 sides to a die)

---
+++ A2_DiceeGame_Dabrowski/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_DiceeGame_Dabrowski
+{
+    class Dice
+    {
+        public int Face { get; }
+        public int Max { get; set; }
+
+        public Dice()
+        {
+            Max = 6;
+        }
+    }
+}
