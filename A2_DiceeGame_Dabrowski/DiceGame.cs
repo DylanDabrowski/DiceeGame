05fabe0995a6a2117c590bc5f1c0cd6c32255934
Dylan Dabrowski <dabrowsd@shernet.sheridancollege.ca>
2020-04-02 17:32:37 -0400

    class DiceGame has been made with properties and empty methods.

---
+++ A2_DiceeGame_Dabrowski/DiceGame.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_DiceeGame_Dabrowski
+{
+    class DiceGame
+    {
+        public List<Dice> Dice { get; set; }
+        public List<Player> Players { get; set; }
+        public Player ActivePlayer { get; }
+        public bool IsGameOver { get; }
+
+        public DiceGame(int numberOfDice)
+        {
+            Players = new List<Player>();
+
+        }
+
+        public void AddPlayer(Player player)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void PlayTurn()
+        {
+
+        }
+
+        public void RollDice()
+        {
+
+        }
+
+        public void ComputeTurnResult()
+        {
+
+        }
+
+        public void UpdatePlayerStat()
+        {
+
+        }
+
+        public void SetNextPlayer()
+        {
+
+        }
+
+        public Player TheWinner()
+        {
+            return ActivePlayer; //temp, just to get rid of red line
+        }
+
+
+    }
+}
